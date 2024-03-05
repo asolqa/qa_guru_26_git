b5e30d4d7b14f624ba76e9eca8805462dd6c431d
Anna Solyanskaya <ASolyanskaya@it-one.ru>
2024-03-05 22:46:25 +0300

    added Delta

--- src/main/java/org/qaasol/Main.java
+++ src/main/java/org/qaasol/Main.java
@@ -3,5 +3,6 @@ package org.qaasol;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello qa guru!");
+        System.out.println("Delta from slow_work branch");
     }
 }
